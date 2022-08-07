28917c992c4f90a7c04dafc0ef7750edff2db6e5
Kareem Hesham <85392795+Kareemhesham2005@users.noreply.github.com>
2022-08-07 02:51:14 +0200

    Finished Services section

--- JS/main.js
+++ JS/main.js
@@ -9,3 +9,20 @@ const changeBgHeader = () => {
 };
 
 window.addEventListener("scroll", changeBgHeader);
+
+// Services Card More Details
+
+const seeMore = document.querySelectorAll(".services_button");
+const modalActiveClass = "services_modal-active";
+const closeIcons = document.querySelectorAll(".services_modal-close");
+const servicesModals = document.querySelectorAll(".services_modal");
+
+servicesModals.forEach((modal, i) => {
+  seeMore[i].addEventListener("click", () => {
+    modal.classList.add(modalActiveClass);
+  });
+
+  closeIcons[i].addEventListener("click", () => {
+    modal.classList.remove(modalActiveClass);
+  });
+});
