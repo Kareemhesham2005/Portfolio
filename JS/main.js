593261727d59103bbdf3d51cca7e0a7ced81dbf2
Kareem Hesham <85392795+Kareemhesham2005@users.noreply.github.com>
2022-08-08 06:02:09 +0200

    Finished Work Section

--- JS/main.js
+++ JS/main.js
@@ -26,3 +26,32 @@ servicesModals.forEach((modal, i) => {
     modal.classList.remove(modalActiveClass);
   });
 });
+
+// Work Filter
+
+const filters = document.querySelectorAll(".work_item");
+const cards = document.querySelectorAll(".work_card");
+
+filters.forEach((f) => {
+  f.addEventListener("click", () => {
+    filters.forEach((fi) => {
+      fi.classList.remove("work_item-active");
+    });
+
+    const type = f.attributes["data-type"].value;
+    f.classList.add("work_item-active");
+
+    cards.forEach((c) => {
+      if (c.attributes["data-type"].value === type) {
+        c.classList.add("work_card-active");
+        c.style.animation = "portfolio 0.3s 1 linear";
+      } else if (type === "all") {
+        c.style.animation = "";
+        c.classList.add("work_card-active");
+      } else {
+        c.classList.remove("work_card-active");
+        c.style.animation = "";
+      }
+    });
+  });
+});
